3f84d30965ae950f5b39b40aeabd41388dc8f362
vincent <vincent.faye@koparexpress.com>
2020-09-23 15:49:20 +0000

    add some styles to user card

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="user" v-if="user">
-    <div class="user-author-avatar" v-if="img">
+  <div id="user" class="user" v-if="user">
+    <div style="margin-left: .4rem;" class="user-author-avatar" v-if="img">
       <img :src="img" :alt="user.name"/>
     </div>
     <div class="user-details flex">
@@ -14,7 +14,7 @@
         <div class="user-username">{{ user.address.zipcode }}</div>
       </div>
       <div class="user-meta">
-        <div class="user-extra">Extra info</div>
+        <div class="user-extra" style="text-align: center;">Extra info</div>
         <div class="user-extra">website : <a href="#" target="_blank">{{ user.website }}</a></div>
         <div class="user-extra">Company : {{ user.company.name }}</div>
         <div class="user-extra">{{ user.company.catchPhrase }}</div>
@@ -39,6 +39,13 @@ export default {
 
 <style lang="scss" scoped>
   $accent-color: #ff9800;
+  #user {
+    background: #fff;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  #user:hover {
+    background-color: lighten($accent-color, 30);
+  }
   .user {
     --avatar-size: 80px;
     display: flex;
